af8dd10fab8d258de9f1f23c2c8a2b31096b1f0b
yvanroan <yvanroan@yahoo.fr>
2024-11-26 01:15:02 -0500

    laid down the basis for game class

--- game_x.h
+++ game_x.h
@@ -12,7 +12,7 @@ class Game: public Subject {
     vector<*Player> players;
 
     public:
-        enum {Blank=0, LinkBoost, Firewall, Download, Polarize, Scan, MadeInHaven, KingCrimson, Z}; // Available abilities.
+        enum {Blank=0, LinkBoost, Firewall, Download, Polarize, Scan, Z, KingCrimson, MadeInHaven}; // Available abilities.
         explicit Studio(Board *theboard ): theboard{theboard} {}
 
         Board*& board() { return theboard; }
@@ -20,9 +20,13 @@ class Game: public Subject {
         void ability_to_link(int turn,char link_name, int ability_id);
         void render();
         void animate(int turns);
-
         char getState(int row, int col) const override;
   
 };
 
+                //if the square has an ability
+                // if(link.ability.getId()!= 8){
+                //     p.apply_opponent_ability(abilityAt(dx,dy));
+                // }
+                // //if the link is(has) madeInHaven(ability_id = 8) he is untoucheable aginst abilities
 #endif
