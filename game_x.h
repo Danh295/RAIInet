472f28ecd569752f981c9b6a5ed250c24f46b6e3
yvanroan <yvanroan@yahoo.fr>
2024-11-26 16:29:35 -0500

    added setupPlayer and isValidSetup to the game class

--- game_x.h
+++ game_x.h
@@ -14,12 +14,24 @@ class Game: public Subject {
     public:
         enum {Blank=0, LinkBoost, Firewall, Download, Polarize, Scan, Z, KingCrimson, MadeInHaven}; // Available abilities.
         explicit Studio(Board *theboard ): theboard{theboard} {}
+        //this is just to make it easier 
+        enum class abilities {Blank=0, LinkBoost, Firewall, Download, Polarize, Scan, Z, KingCrimson, MadeInHaven}; // Available abilities.
+        explicit Studio(Board *theboard,  vector<*Player> input): theboard{theboard} {
+            i=0
+            for(auto p: input){
+                players[i] = std::move(p);
+                i+=1;
+            }
+        }
 
         Board*& board() { return theboard; }
+        void setupPlayer(int turn, vector<*Ability> abilities, vector<*Link> links);
+        bool isValidSetup();//checks if everything is fine before beginnig turns in the game, if not throws something
         void ability_to_square(int turn,  int x, int y, int ability_id);
         void ability_to_link(int turn,char link_name, int ability_id);
         void render();
         void animate(int turns);
+        void animate(int turn);
         char getState(int row, int col) const override;
   
 };
